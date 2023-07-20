0abe33c354e1b22394ed573a5259e0445d9df231
Kevin Agus <kevin.agus@alten.it>
2023-07-20 12:28:16 +0200

    fix: solution 26

--- src/05-external-libraries/26-usage-with-zod.problem.ts
+++ src/05-external-libraries/26-usage-with-zod.problem.ts
@@ -1,11 +1,11 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const makeZodSafeFunction = (
-  schema: unknown,
-  func: (arg: unknown) => unknown
+const makeZodSafeFunction = <TArg, TFuncRes>(
+  schema: z.Schema<TArg>,
+  func: (arg: TArg) => TFuncRes
 ) => {
-  return (arg: unknown) => {
+  return (arg: TArg) => {
     const result = schema.parse(arg);
     return func(result);
   };
